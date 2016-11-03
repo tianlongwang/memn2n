f755424b2757a5cc80511967772c4a5f61424191
Tianlong Wang <tianlong.wang.13@googlemail.com>
2016-11-03 19:29:47 -0400

    new demo data, use customised data

--- mc_web-demo/static/js/template/story-row-template.js
+++ mc_web-demo/static/js/template/story-row-template.js
@@ -3,12 +3,12 @@ define([], function() {
     return '<div class="col-sm-6 col-md-4">' +
             '<div class="thumbnail">' +
               '<div class="caption">' +
+                '<p><a href="#" class="btn btn-primary" role="button">Select</a></p>' +
                 '<h5>Story</h5>' +
                 '<div class="well"><%= story %></div>' +
-                '<h5>Question: <%= question %></h5><br>' +
-                '<h5>Choices: <%= choices %></h5><br>' +
+                '<h5>Question: <%= question %></h5>' +
+                '<h5>Choices: <%= choices %></h5>' +
                 '<h5>Correct Answer: <%= answer%></h5>' +
-                '<p><a href="#" class="btn btn-primary" role="button">Select</a></p>' +
               '</div>' +
             '</div>' +
           '</div>'
